e68838f6ea3a8e9e7bff0021a043e9e17d0f5865
Erik Bohne <eriknbohne@gmail.com>
2023-09-02 13:07:34 +0200

    task 15

--- ERT3/task_15.py
+++ ERT3/task_15.py
@@ -3,8 +3,8 @@ Program to plot results from a totem (transistors)
 """
 import matplotlib.pyplot as plt
 
-Vq = [21, 27, 100, 300, 416, 514, 600, 646] # mV
-Va = [0, 0.772, 1.112, 1.608, 2.02, 2.42, 2.76, 3.95] # V
+Vq = [7.89, 7.89, 7.89, 7.88, 7.86, 7.76, 7.47, 2.96, 0.066, 0.005] # mV
+Va = [0.5, 0.750, 1.00, 1.250, 1.500, 1.750, 2.0, 2.25, 2.32, 2.5] # V
 
 plt.plot(Vq, Va, 'ro')
 plt.xlabel('Vq (mV)')
